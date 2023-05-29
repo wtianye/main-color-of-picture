ef371a52c99a5889491d4bde0f54a86122e2a7cd
Henry.Wang <Henry.Wang@fanruan.com>
2023-05-29 11:34:56 +0800

    feat: 提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
+import { useMemo, useState } from "react";
+import ColorThief from "colorthief";
+
+const createImageSeed = () => {
+  return Math.round(Math.random() * 10000);
+};
+
 export function App() {
+  const [themeColors, setThemeColors] = useState<string[] | null>(null);
+  const [imageSeeds] = useState(
+    new Array(3).fill(0).map(() => createImageSeed())
+  );
+
+  const backgroundStyle = useMemo(() => {
+    return {
+      background:
+        themeColors !== null
+          ? `linear-gradient(${themeColors.join(",")})`
+          : "white",
+    };
+  }, [themeColors]);
+
+  const handleMouseEnter = (e: any) => {
+    const colorThief = new ColorThief();
+    const img = e.target;
+    const colors = colorThief.getPalette(img, 3);
+    const rgbColors: string[] = [];
+    colors.forEach((color) => {
+      const rbg = `rgb(${color[0]},${color[1]},${color[2]})`;
+      rgbColors.push(rbg);
+    });
+    setThemeColors(rgbColors);
+  };
+
+  const handleMouseLeave = () => {
+    setThemeColors(null);
+  };
+
   return (
-    <div className="h-full w-full flex items-center justify-center ">
+    <div
+      className="h-full w-full flex items-center justify-center "
+      style={backgroundStyle}
+    >
       <div className="grid gap-20 grid-cols-3 cursor-pointer">
-        {new Array(3).fill(0).map((_, index) => {
+        {imageSeeds.map((seed, index) => {
           return (
             <img
               style={{
@@ -10,7 +50,10 @@ export function App() {
               }}
               key={index}
               alt="图片"
-              src={`https://picsum.photos/800/800`}
+              src={`https://picsum.photos/seed/${seed}800/800`}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              crossOrigin=""
             ></img>
           );
         })}
---
+++ src/colorthief.d.ts
@@ -0,0 +1,6 @@
+declare module "colorthief" {
+  export default class ColorThief {
+    constructor();
+    getPalette(ing: any, count: number): any[];
+  }
+}
